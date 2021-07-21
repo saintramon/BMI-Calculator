893c387d2d02291f0c9ac1a554d9068467593984
Ramon Emmiel Jasmin <ramonjasmin8@gmail.com>
2021-07-21 16:47:31 +0800

    Added animations, functionalities soon to be added

---
+++ codes/script.js
@@ -0,0 +1,21 @@
+var kg = document.getElementById("kilograms")
+var lbs = document.getElementById("pounds")
+var btn = document.getElementById("btn")
+var height = document.getElementsByClassName("height")
+
+function calculate(){
+    var bmi;
+}
+
+
+function pounds(){
+ kg.style.left = "-300px"
+ lbs.style.left = "0px"
+ btn.style.left = "118px"
+}
+function kilograms(){
+    kg.style.left = "0px"
+    lbs.style.left = "300px"
+    btn.style.left = "0px"
+}
+    

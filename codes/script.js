6d0ba29b3b0a570610ba9f589af8a8d1e4fd3dce
Ramon Emmiel Jasmin <ramonjasmin8@gmail.com>
2021-07-27 10:06:09 +0800

    added functions, not yet complete

--- codes/script.js
+++ codes/script.js
@@ -3,15 +3,30 @@ var lbs = document.getElementById("pounds")
 var btn = document.getElementById("btn")
 var height = document.getElementsByClassName("height")
 
-function calculate(){
-    var bmi;
-}
+
 
 
 function pounds(){
  kg.style.left = "-300px"
  lbs.style.left = "0px"
  btn.style.left = "118px"
+
+ function calcpounds(){
+     var heightLbs = document.getElementById("heightLbs").nodeValue;
+     var weightLbs = document.getElementById("weightLbs").nodeValue;
+
+     let finalWeightlbs = weightLbs * 0.453592;
+     let finalHeightlbs = heightLbs * 0.01;
+
+     let finalBMILbs = finalWeightlbs / (finalHeightlbs)^2;
+
+     if (finalBMILbs < 18.5){
+         
+     }
+
+     
+
+ }
 }
 function kilograms(){
     kg.style.left = "0px"
